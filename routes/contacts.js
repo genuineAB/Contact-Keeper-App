3cca7968d172529e8ba0f9a48bc7ba20aa42f7c1
genuineAB <bamigboyeabiola@gmail.com>
2022-07-16 17:11:36 +0100

    Created the contact delete route

--- routes/contacts.js
+++ routes/contacts.js
@@ -102,8 +102,26 @@ router.patch('/:id', auth, async (req, res) => {
 //@route    DELETe api/contacts/:id 
 //@desc     Delete a contact
 //@access   Private
-router.delete('/:id', auth, (req, res) => {
-    res.send('Delete a contact');
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if(!contact){
+            return res.status(404).json({msg: "Contact not found"})
+        }
+
+        // Make sure user owns contact
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'Not authorized' });
+        }
+
+        await Contact.findByIdAndDelete(req.params.id);
+
+        res.json({msg: "Contact deleted"});
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
 });
 
 
