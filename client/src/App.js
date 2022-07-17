e4d84dbd9ede95b17730285341f31b140ed480e7
genuineAB <bamigboyeabiola@gmail.com>
2022-07-17 19:55:15 +0100

    Setup Frontend and Worked on Navbar

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css';
+import { Home } from './components/pages/Home';
+import { About } from './components/pages/About';
+import { Navbar } from './components/layout/Navbar';
+// import { Fragment } from 'react';
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="App">
+          <Routes>
+            <Route exact path='/' element={<Home />}/>
+            <Route exact path='/about' element={<About />}/>
+          </Routes>
+          
+        </div>
+      </Fragment>
+    </Router>
+  );
+}
+
+export default App;
