c5e0835ba97bdc1b831cf429d5d5a345671b0287
genuineAB <bamigboyeabiola@gmail.com>
2022-07-22 23:06:32 +0100

    Created the delete contact UI

--- client/src/components/contact/ContactsItem.js
+++ client/src/components/contact/ContactsItem.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
+
 
 const ContactsItem = ({contact}) => {
-    const { name, email, phone, type} = contact;
+    const contactContext = useContext(ContactContext);
+    const {deleteContact} = contactContext;
+
+    const { id, name, email, phone, type} = contact;
     // console.log(contact.id);
+    const onDelete = () => {
+        deleteContact(id);
+    }
   return (
     <div className='card bg-light'> 
         <h3 className='text-primary text-left'>
@@ -23,7 +31,7 @@ const ContactsItem = ({contact}) => {
         </ul>
         <p>
             <button className='btn btn-dark btn-sm'>Edit</button>
-            <button className='btn btn-danger btn-sm'>Delete</button>
+            <button className='btn btn-danger btn-sm' onClick={onDelete}>Delete</button>
         </p>
     </div>
   )
