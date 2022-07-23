89ec816407c2aafa76f9c8ce7afc5c7d9de0ef01
genuineAB <bamigboyeabiola@gmail.com>
2022-07-23 16:51:46 +0100

    Added the initiatl setup the Authentication Context

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,28 @@ import './App.css';
 import { Home } from './components/pages/Home';
 import { About } from './components/pages/About';
 import { Navbar } from './components/layout/Navbar';
-import ContactState from './context/contact/ContactState'
+import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="App">
-            <Routes>
-              <Route exact path='/' element={<Home />}/>
-              <Route exact path='/about' element={<About />}/>
-            </Routes>
-            
-          </div>
-        </Fragment>
-      </Router>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="App">
+              <Routes>
+                <Route exact path='/' element={<Home />}/>
+                <Route exact path='/about' element={<About />}/>
+              </Routes>
+              
+            </div>
+          </Fragment>
+        </Router>
     </ContactState>
+    </AuthState>
+    
   );
 }
 
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,11 +1,53 @@
 import React, {useReducer} from 'react';
 import AuthContext from './authContext';
-import { Reducer  } from 'react';
+import AuthReducer from './authReducer';
+import {
+     SET_ALERT,
+     REMOVE_ALERT,
+     REGISTER_SUCCESS,
+     REMOVE_FAIL,
+     USER_LOADED,
+     AUTH_ERROR,
+     LOGIN_SUCCESS,
+     LOGIN_FAIL,
+     CLEAR_ERRORS
 
-const AuthState = () => {
+    
+    } from '../types'; 
+
+const AuthState = props => {
+    const initialState = {
+        token: localStorage.getItem('token'),
+        isAuthenticated: null,
+        loading: true,
+        error: null
+    };
+
+    const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+    //Load User
+
+    //Register User
+
+    //Login User
+
+    //Logout
+
+    //Clear Errors
+
   return (
-    <div>AuthState</div>
+    <AuthContext.Provider 
+        value={{
+            token: state.token,
+            isAuthenticated: state.isAuthenticated,
+            loading: state.loading,
+            user: state.user,
+            error: state.error
+
+        }}>
+            {props.children}
+    </AuthContext.Provider>
   )
-}
+} 
 
 export default AuthState
