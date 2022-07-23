96a0b6ba6342816630e73a4066dbb08943e09537
genuineAB <bamigboyeabiola@gmail.com>
2022-07-23 17:48:58 +0100

    Added the Register and Login in Forms

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import { Home } from './components/pages/Home';
 import { About } from './components/pages/About';
 import { Navbar } from './components/layout/Navbar';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 
@@ -18,7 +20,10 @@ const App = () => {
               <Routes>
                 <Route exact path='/' element={<Home />}/>
                 <Route exact path='/about' element={<About />}/>
+                <Route exact path='/register' element={<Register />}/>
+                <Route exact path='/login' element={<Login />}/>
               </Routes>
+              
               
             </div>
           </Fragment>
