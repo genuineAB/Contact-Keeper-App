2ed304cfabb435e07db34fe8d4f4b29f11456614
genuineAB <bamigboyeabiola@gmail.com>
2022-07-31 02:46:50 +0100

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Create an instance of a server
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 //Connect DB
@@ -18,5 +19,13 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    //
+    app.use(express.static('clien/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
